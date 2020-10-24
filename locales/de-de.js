36b1d3644f4eae64a71f4fcd31e63e099a1d7c03
Ibot02 <jtp.jtp@gmx.de>
2020-10-24 22:53:19 +0200

    German Translation

---
+++ locales/de-de.js
@@ -0,0 +1,17 @@
+module.exports = {
+  nav: {
+    links: {
+      rando: 'Erstelle eine ROM',
+      github: 'Github',
+      discord: 'Discord',
+      about: 'Info',
+      tracker: 'Tracker',
+    },
+  },
+  about: {
+    shoutout: 'Dankeschön an alle Entwickler und Tester, die dieses Projekt ermöglicht haben! (Vollständige Danksagung folgt später)',
+  },
+  index: {
+    greeting: 'Minish Cap Rando! Hurrah!',
+  },
+};
